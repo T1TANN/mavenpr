8ba18eaa89d7c850c09715417b11b4a231396576
t1tan ultron <Abuzargore91@gmail.com>
2023-11-05 21:41:13 +0530

    done

---
+++ my-app/src/main/java/com/mycompany/app/SeleniumExample.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.app;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumExample {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.example.com%22/");
+
+        driver.quit();
+    }
+}
